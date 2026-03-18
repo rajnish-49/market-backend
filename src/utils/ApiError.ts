8100529680637cc177e92586b0ba590b391e6fd3
Rajnish Kumar <rajnish.23fe10ite00090@muj.manipal.edu>
2026-03-18 15:42:09 +0530

    main express applcation

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,14 +1,15 @@
 export class ApiError extends Error {
+  // inherits from the built in error class ( message , stack trace etc )
   public readonly statusCode: number;
   public readonly isOperational: boolean;
 
 
   constructor(statusCode: number, message: string, isOperational = true) {
-    super(message);
+    super(message); // calls the parent constructor (Error) with the message eq to Error(message)
     this.statusCode = statusCode;
     this.isOperational = isOperational;
     
-    Object.setPrototypeOf(this, new.target.prototype);
+    Object.setPrototypeOf(this, new.target.prototype); // TypeScript/JavaScript inheritance fix. Ensures that the prototype chain is correctly set up for instances of ApiError, allowing instanceof checks to work properly.
     Error.captureStackTrace(this);
   }
 }
