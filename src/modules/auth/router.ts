79cb951b8b2a9faa90dc4ec1e984b790323e776d
Rajnish Kumar <rajnish.23fe10ite00090@muj.manipal.edu>
2026-03-18 15:41:00 +0530

    Auth modules

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -1,1 +1,11 @@
-export {};
+import { Router } from "express";
+import { validate } from "../../middlewares/validate";
+import { registerSchema, loginSchema } from "./types";
+import { register, login } from "./controller";
+
+const router = Router();
+
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
+
+export default router;
