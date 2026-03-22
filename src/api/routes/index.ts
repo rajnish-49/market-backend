e83971bcd59ac8fdc8bd8eb546a38e138247265d
Rajnish Kumar <rajnish.23fe10ite00090@muj.manipal.edu>
2026-03-22 20:21:16 +0530

    product module

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import authRouter from "../../modules/auth/router";
+import categoryRouter from "../../modules/categories/router";
+import productRouter from "../../modules/products/router";
 
 export const apiRouter = express.Router();
 
 apiRouter.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/categories", categoryRouter);
+apiRouter.use("/products", productRouter);
